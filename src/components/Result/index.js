9edd35b0c205664d635d7bdc7e88cb187df8ac4b
natbibi <58271566+natbibi@users.noreply.github.com>
2021-04-21 16:51:50 +0100

    add styling, add icons and change text

--- src/components/Result/index.js
+++ src/components/Result/index.js
@@ -2,28 +2,35 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { selectRepo } from '../../actions'
 import { useDispatch } from 'react-redux'
+import { GoGitBranch, GoTelescope, GoIssueOpened } from "react-icons/go";
 
-function Item({ repo }){
+
+
+
+function Item({ repo }) {
     const dispatch = useDispatch()
-    
-    function handleClick(repo){
+
+    function handleClick(repo) {
         console.log(repo)
         dispatch(selectRepo(repo))
     }
 
-    return(
+    return (
         <div className="repo-container">
-        <h3>{repo.name}</h3><Link onClick={() => handleClick(repo.name)} to={'/info'}>more info...</Link>
-        <p>Forks: {repo.forks}</p>
-        <p>Stargazers: {repo.stargazers_count}</p>
+            <h3>{repo.name}</h3><Link onClick={() => handleClick(repo.name)} to={'/info'}>see collaborators...</Link>
+            <div className="git-icons-container">
+                <p><GoGitBranch /> {repo.forks}</p>
+                <p><GoTelescope /> {repo.stargazers_count}</p>
+                <p><GoIssueOpened /> {repo.open_issues}</p>
+            </div>
         </div>
     )
 }
 
-function Result({result}){
-    const resultList = result.map(item => <Item repo={item}/>)
-    return(
-        <ul>{resultList}</ul>
+function Result({ result }) {
+    const resultList = result.map(item => <Item repo={item} />)
+    return (
+        <ul className="result-container">{resultList}</ul>
     )
 }
 
