580343eab43c099f16fd4cc05dcc66cc8ea4a977
chris westerduin <chriswesterduin@outlook.com>
2021-04-21 12:15:10 +0100

    format result and add router

--- src/App.js
+++ src/App.js
@@ -1,40 +1,20 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { SearchForm, Result, Toggle, Avatar } from './components';
-import { getResult } from './actions';
-import {Helmet} from "react-helmet";
+import { Toggle } from './components'
+import { Home, Info } from './pages'
+import { Switch, Route } from 'react-router-dom'
 import './style.css'
-function Search(){
 
-    const result = useSelector(state => state.user.repos);
-    const mode = useSelector(state => state.mode);
+function App(){
 
-    const username = useSelector(state => state.user.name);
-    const loading = useSelector(state => state.loading);
-    const error = useSelector(state => state.error)
-
-    const dispatch = useDispatch();
-    
-    const search = searchTerm => dispatch(getResult(searchTerm));
-
-    const renderResult = () => loading ? <p>Loading . . .</p> : <Result result={result}/>
-return (
-        <>
-        <Toggle/>
-        <p>toggle? {JSON.stringify(mode)}</p>
-        <div id="search">
-            <SearchForm getResult={search}/>
-
-        <div className="user-info">
-            <h1>{username && `Stalking: ${username}`}</h1>
-            {result.length > 0 ? <Avatar url={result[0].owner.avatar_url} /> : null }
-        </div>
-            { error ? <p role="alert">Oops there's been an error! {error}</p> : renderResult() }   
-            
-        </div>
-        </>
+    return (
+      <>
+        <Toggle />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/info" component={Info} />
+        </Switch>
+      </>
     );
-
 }
 
-export default Search;
+export default App;
--- src/components/Result/index.js
+++ src/components/Result/index.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { selectRepo } from '../../actions'
+import { useDispatch } from 'react-redux'
 
 function Item({ repo }){
- 
+    const dispatch = useDispatch()
+    
+    function handleClick(repo){
+        console.log(repo)
+        dispatch(selectRepo(repo))
+    }
+
     return(
         <div className="repo-container">
-        <h3>{repo.name}</h3>
+        <h3>{repo.name}</h3><Link onClick={() => handleClick(repo.name)} to={'/info'}>more info...</Link>
         <p>Forks: {repo.forks}</p>
         <p>Stargazers: {repo.stargazers_count}</p>
         </div>
